4fe1e6e25628c1ceac844997904e2684321770da
lin_h <lin_h@DEV>
2024-06-28 19:08:21 +0800

    添加项目文件。

---
+++ 系统模块/服务器组件/游戏控制/stdafx.h
@@ -0,0 +1,38 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////////////
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN
+#endif
+
+#ifndef WINVER
+#define WINVER 0x0501
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501
+#endif
+
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS 0x0501
+#endif
+
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0501
+#endif
+
+#define _AFX_ALL_WARNINGS
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS
+
+//////////////////////////////////////////////////////////////////////////////////
+
+//MFC 文件
+#include <AfxWin.h>
+#include <AfxExt.h>
+#include <AfxCmn.h>
+#include <AfxDisp.h>
+#include <AfxDtctl.h>
+//////////////////////////////////////////////////////////////////////////////////
+
+#include "..\游戏服务\GameServiceHead.h"
